14ad9fe83535897a1ac6ca0b92249298c5464e9b
howelrob <howellrobbie01@gmail.com>
2025-10-08 16:56:37 -0500

    add code to save the output and the end and to read in the arguments from the command line

--- code/hpc_script.R
+++ code/hpc_script.R
@@ -1,17 +1,33 @@
 rm(list = ls())
 library(spOccupancy)
 
-#set working directory to where all the files are stored
-#setwd("C:/Users/howel/OneDrive/Documents/GitHub/Doser_Howell_Occupancy_Research/code")
+
+#read in given arguments (three numbers, e.g. (first row, last row, replicates))
+args <- commandArgs(trailingOnly = TRUE)
+
+start_row = as.numeric(args[1])
+end_row = as.numeric(args[2])
+replicates = as.numeric(args[3])
+
 
 #load utils methods and parameters csv
 source("utils.R")
 parameters = read.csv("parameters.csv")
 
+#set directories, unsure how to read in the utils and parameters csv through the HPC
+machine.name <- Sys.info()['nodename']
+if (machine.name == 'pop-os') {
+  #out.dir <- 'results/occupancy_sampling_results/'
+  #data.dir <- 'data/sim_data_replicates/'
+} else { # Running on NCSU HPC
+  #out.dir <- '/share/doserlab/jwdoser/F25/results/occupancy_sampling_results/'
+  #data.dir <- '/share/doserlab/jwdoser/F25/data/sim_data_replicates/'
+}
+
 
 #parameter rows and the number of iterations can easily be changed
-current_params = parameters[1:2,]
-n_iters = 5
+current_params = parameters[start_row:end_row,]
+n_iters = replicates
 
 #loop through each row of the given parameters
 for (i in 1:nrow(current_params)){
@@ -25,5 +41,7 @@ for (i in 1:nrow(current_params)){
     output = data_simulation(n.neighbors=neighbors, method=design, species_prev=prevalence, spatial_decay=decay)
     
     #Need to save the output to a file
+    save(output, file=paste0(out.dir, 'occupancy-sampling-row-', i, '-replicate-', j, '-', 
+                             Sys.Date(), '.rda'))
   }  
 }
